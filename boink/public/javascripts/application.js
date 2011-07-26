dc1cba8bea0995856ddc8d2e276767206045d7ba
Andrew Hao <andrewhao@gmail.com>
2011-07-25 23:13:18 -0700

    Added some basic JS to fire off a /start_snap.

--- boink/public/javascripts/application.js
+++ boink/public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(window).ready(function () {
+    $('button#start-button').click(function(e) {
+        $.get('start_snap', null, function(data) {
+            // Temp logging
+            console.log("set_id is: "+data.set_id);
+            console.log("timestamps are: "+data.timestamps);
+            
+            // TODO/andrew Implementation
+        })
+    });
+})
