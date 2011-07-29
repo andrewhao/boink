96847a4c39358a8f3f63477060b73aa6832d20de
Andrew Hao <andrewhao@gmail.com>
2011-07-29 13:01:32 -0700

    Draw four photos and place them accordingly.

--- boink/public/javascripts/application.js
+++ boink/public/javascripts/application.js
@@ -1,10 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-var PHOTO_PADDING = 50; // Padding for the grand photo per side
+var PHOTO_PADDING = 50; // Margin for the grand photo per side
 var WINDOW_WIDTH = $(window).width();
 var WINDOW_HEIGHT = $(window).height();
-var PHOTO_PADDING = 50; // Padding on the sides and down the middle of the photo 2x2.
 
 // Given a max w and h bounds, return the dimensions
 // of the largest 4x6 rect that will fit within.
@@ -29,9 +28,31 @@ PhotoView.prototype.render = function() {
     var w = WINDOW_WIDTH - PHOTO_PADDING;
     var h = WINDOW_HEIGHT - PHOTO_PADDING;
     var scaled = scale4x6(w, h);
-    var r = this.canvas.rect((WINDOW_WIDTH - scaled.w) / 2, (WINDOW_HEIGHT - scaled.h) / 2, scaled.w, scaled.h);
+    var photo_x = (WINDOW_WIDTH - scaled.w) / 2;
+    var photo_y = (WINDOW_HEIGHT - scaled.h) / 2;
+    var r = this.canvas.rect(photo_x, photo_y, scaled.w, scaled.h);
     r.attr({'fill': 'white'});
     
+    // Scale the photo padding too
+    var PHOTO_BORDER = scaled.w / 50;
+    
+
+    //upper left
+    var frame_x = photo_x + PHOTO_BORDER;
+    var frame_y = photo_y + PHOTO_BORDER;
+    var frame_w = (scaled.w - (3*PHOTO_BORDER))/2;
+    var frame_h = (scaled.h - (3*PHOTO_BORDER))/2;
+    var frame = this.canvas.rect(frame_x, frame_y, frame_w, frame_h);
+    frame.attr({'fill': 'black'});
+    
+    frame = frame.clone();
+    frame.translate(frame_w + PHOTO_BORDER, 0);
+    
+    frame = frame.clone();
+    frame.translate(0, frame_h + PHOTO_BORDER);
+    
+    frame = frame.clone();
+    frame.translate(-(frame_w + PHOTO_BORDER), 0);
 
 }
 
