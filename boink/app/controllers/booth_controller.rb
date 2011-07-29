b4d16d15b8e0614b748f1494c3a6bf9c60b15cae
Andrew Hao <andrewhao@gmail.com>
2011-07-29 00:59:48 -0700

    More client timing fixes.

--- boink/app/controllers/booth_controller.rb
+++ boink/app/controllers/booth_controller.rb
@@ -2,19 +2,29 @@ require 'json'
 
 class BoothController < ApplicationController
   PHOTO_COUNT = 4 # number of photos that will be taken
-  PHOTO_PREDELAY = 5 # delay before first photo is taken
-  PHOTO_DELAY = 5 # delay between photos being taken
+  PHOTO_PREDELAY = 0 # delay before first photo is taken
+  PHOTO_DELAY = 10 # delay between photos being taken
   
   def show
+  end
+  
+  def time_now
+    @response = {}
+    @response[:timestamps] = []
+    start_time = (PHOTO_PREDELAY + Time.now.to_i) * 1000
+    PHOTO_COUNT.times do |i|
+      @response[:timestamps] << start_time + (PHOTO_DELAY * i * 1000)
+    end
+    render :json => @response
   end
   
   def start_snap
     @response = {}
     @response[:set_id] = 0
     @response[:timestamps] = []
-    start_time = Time.now.to_i + PHOTO_PREDELAY * 1000
+    start_time = (Time.now.to_i + PHOTO_PREDELAY) * 1000
     PHOTO_COUNT.times do |i|
-      @response[:timestamps] << start_time + PHOTO_DELAY * 1000 * (i + 1)
+      @response[:timestamps] << start_time + (PHOTO_DELAY * 1000 * i)
     end
     # call call_rake to call script to take photos from here, passing in starting timestamp
     # and delta so that the camera can start doing work
--- boink/public/javascripts/application.js
+++ boink/public/javascripts/application.js
@@ -1,14 +1,52 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var timestamps = null;
+
+function countdown(expected) {
+    var counter = 3;
+    var countdownTimer = setInterval(function() {
+        console.log(counter);
+        if (counter == 1) {
+            console.log('expected to snap: ' + expected);
+            clearInterval(countdownTimer);
+            setTimeout(function() {snap(expected);}, 1000);
+        }
+        counter -= 1;
+    }, 1000);
+}
+
+function snap(expected_time) {
+    var now = (new Date()).getTime();
+    console.log('snap at ' + now);
+    console.log('delta from expected: ' + expected_time - now);
+}
 
 $(window).ready(function () {
     $('button#start-button').click(function(e) {
-        $.get('start_snap', null, function(data) {
+
+        $.get('time_now', null, function(data) {
             // Temp logging
             console.log("set_id is: "+data.set_id);
             console.log("timestamps are: "+data.timestamps);
+
+            // Set global
+            timestamps = data.timestamps;
+            var time_now = (new Date()).getTime();
+            var closest_ts = timestamps.pop();
+            var ts_delta = closest_ts - time_now - 3000;
             
-            // TODO/andrew Implementation
-        })
+            console.log('delta is:' + ts_delta);
+
+            while(timestamps.length > 0) {
+                console.log('timestamps array is'+timestamps);
+                // Start countdown
+                setTimeout(function() {
+                    countdown(closest_ts);
+                }, ts_delta);
+                closest_ts = timestamps.pop();
+                ts_delta = closest_ts - time_now - 3000;
+            }
+
+        });
     });
-})
+});
