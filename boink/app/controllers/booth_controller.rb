6072174f456e5c4243d4ddbce838dd5d1eea1175
Andrew Hao <andrewhao@gmail.com>
2011-07-29 18:31:34 -0700

    Add modal dialogs. Updates the camera files. Mock /photoset action. Frontend now updates photoset.

--- boink/app/controllers/booth_controller.rb
+++ boink/app/controllers/booth_controller.rb
@@ -7,17 +7,9 @@ class BoothController < ApplicationController
   
   def show
   end
-  
-  def time_now
-    @response = {}
-    @response[:timestamps] = []
-    start_time = (PHOTO_PREDELAY + Time.now.to_i) * 1000
-    PHOTO_COUNT.times do |i|
-      @response[:timestamps] << start_time + (PHOTO_DELAY * i * 1000)
-    end
-    render :json => @response
-  end
-  
+
+  # Indicates the client is ready to begin taking photos.
+  # Returns a set of timestamps the camera will click at.
   def start_snap
     @response = {}
     @response[:set_id] = 0
@@ -31,9 +23,22 @@ class BoothController < ApplicationController
     render :json => @response
   end
 
+  # Return the images available in the requested photoset.
+  #--
+  # FIXME Currently returning hardcoded mock data.
   def photoset
-    request = JSON.parse params
-    photo_set = PhotoSet.find request[:set_id].to_i
-    photo_set.paths
+#    photo_set = PhotoSet.find params[:set_id].to_i
+#    photo_set.paths
+
+    # MOCK/andrewhao
+    @response = {}
+    @response[:images] = [
+      {:index => 0, :url => '/images/photos/1.jpg'},
+      {:index => 1, :url => '/images/photos/2.jpg'},
+      {:index => 2, :url => '/images/photos/3.jpg'},
+      {:index => 3, :url => '/images/photos/4.jpg'},
+    ]
+    render :json => @response
+
   end
 end
--- boink/public/javascripts/application.js
+++ boink/public/javascripts/application.js
@@ -1,13 +1,141 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-var PHOTO_PADDING = 50; // Margin for the grand photo per side
+var PHOTO_MARGIN = 50; // Margin for the composite photo per side
 var WINDOW_WIDTH = $(window).width();
 var WINDOW_HEIGHT = $(window).height();
 
-// Given a max w and h bounds, return the dimensions
-// of the largest 4x6 rect that will fit within.
-function scale4x6(maxw, maxh) {
+var currentPhotoSet = {};
+
+
+function PhotoView() {
+    this.container = $('#viewport');
+    this.canvas = new Raphael('viewport', $(window).width(), $(window).height());
+    this.frames = this.canvas.set(); // List of SVG images (photos).
+}
+
+PhotoView.prototype.render = function() {
+    var w = WINDOW_WIDTH - PHOTO_MARGIN;
+    var h = WINDOW_HEIGHT - PHOTO_MARGIN;
+    var scaled = CameraUtils.scale4x6(w, h);
+    var photo_x = (WINDOW_WIDTH - scaled.w) / 2;
+    var photo_y = (WINDOW_HEIGHT - scaled.h) / 2;
+    var r = this.canvas.rect(photo_x, photo_y, scaled.w, scaled.h);
+    r.attr({'fill': 'white'});
+    
+    // Scale the photo padding too
+    var PHOTO_BORDER = scaled.w / 50;
+
+    //upper left
+    var frame_x = photo_x + PHOTO_BORDER;
+    var frame_y = photo_y + PHOTO_BORDER;
+    var frame_w = (scaled.w - (3*PHOTO_BORDER))/2;
+    var frame_h = (scaled.h - (3*PHOTO_BORDER))/2;
+    var frame = this.canvas.rect(frame_x, frame_y, frame_w, frame_h);
+    //var frame = this.canvas.image(null, frame_x, frame_y, frame_w, frame_h);
+    frame.attr({'fill': 'black'});
+
+    this.frames.push(frame);
+    
+    frame = frame.clone();
+    frame.translate(frame_w + PHOTO_BORDER, 0);
+    this.frames.push(frame);
+    
+    frame = frame.clone();
+    frame.translate(0, frame_h + PHOTO_BORDER);
+    this.frames.push(frame);
+    
+    frame = frame.clone();
+    frame.translate(-(frame_w + PHOTO_BORDER), 0);
+    this.frames.push(frame);
+}
+
+/**
+ * Queries the server for updated photos
+ */
+PhotoView.prototype.updatePhotoSet = function(setId) {
+    var view = this;
+    $.get('photoset', {set_id: setId}, function(data) {
+       var images = data.images;
+       for (var i in images) {
+           // New photo
+           if (currentPhotoSet[i] === undefined) {
+               var image = images[i];
+               currentPhotoSet[i] = image;
+               var oldframe = view.frames[i];
+               // Draw new rect
+               var img = view.canvas.image(image.url,
+                    oldframe.attr('x'),
+                    oldframe.attr('y'),
+                    oldframe.attr('width'),
+                    oldframe.attr('height'));
+           }
+       }
+    });
+}
+
+/**
+ * Draws a modal with some text.
+ */
+PhotoView.prototype.modalMessage = function(text, persistTime, animateSpeed) {
+    if (animateSpeed === undefined) { var animateSpeed = 200; }
+    if (persistTime === undefined) { var persistTime = 500; }
+
+    var sideLength = WINDOW_HEIGHT * 0.3;
+    var x = (WINDOW_WIDTH - sideLength)/2;
+    var y = (WINDOW_HEIGHT - sideLength)/2;
+    var all = this.canvas.set();
+    var r = this.canvas.rect(x, y,
+        sideLength,
+        sideLength,
+        10);
+    r.attr({'fill': 'black',
+            'fill-opacity': 0.7,
+            'stroke-color': 'white'});
+    all.push(r);
+    var txt = this.canvas.text(x + sideLength/2, y + sideLength/2, text);
+    txt.attr({'fill': 'white',
+        'font-size': '30',
+        'font-weight': 'bold',
+        'rotation': '-10'});
+    all.push(txt);
+    all.attr({'opacity': 0});
+    all.animate({'opacity': 1, 'rotation': '0', 'scale': '1.5,1.5', 'font-size': '50'}, animateSpeed, '>');
+    
+    setTimeout(function(all) {
+        all.animate({'opacity': 0, 'scale': '1,1', 'rotation': '10', 'font-size': '30'},
+            animateSpeed,
+            '<',
+            function() {
+                // Delete nodes
+                txt.remove();
+                r.remove()
+            });
+    }, persistTime, all);
+
+}
+
+/**
+ * A class of utility methods.
+ */
+function CameraUtils() {};
+
+/**
+ * Play the snap effect.
+ */
+CameraUtils.snap = function(expected_time) {
+    var now = (new Date()).getTime();
+    p.modalMessage('Cheese!');
+    console.log('snap at ' + now);
+    console.log('delta from expected: ' + (expected_time - now));
+    p.updatePhotoSet();
+}
+
+/**
+ * Given a max w and h bounds, return the dimensions
+ * of the largest 4x6 rect that will fit within.
+ */
+CameraUtils.scale4x6 = function(maxw, maxh) {
     var s0 = 6/4; // width / height
     var s1 = maxw/maxh;
     
@@ -19,69 +147,35 @@ function scale4x6(maxw, maxh) {
     }
 }
 
-function PhotoView() {
-    this.container = $('#viewport');
-    this.canvas = new Raphael('viewport', $(window).width(), $(window).height());
-    this.frames = []; // List of SVG images (photos).
-}
-PhotoView.prototype.render = function() {
-    var w = WINDOW_WIDTH - PHOTO_PADDING;
-    var h = WINDOW_HEIGHT - PHOTO_PADDING;
-    var scaled = scale4x6(w, h);
-    var photo_x = (WINDOW_WIDTH - scaled.w) / 2;
-    var photo_y = (WINDOW_HEIGHT - scaled.h) / 2;
-    var r = this.canvas.rect(photo_x, photo_y, scaled.w, scaled.h);
-    r.attr({'fill': 'white'});
-    
-    // Scale the photo padding too
-    var PHOTO_BORDER = scaled.w / 50;
-    
-
-    //upper left
-    var frame_x = photo_x + PHOTO_BORDER;
-    var frame_y = photo_y + PHOTO_BORDER;
-    var frame_w = (scaled.w - (3*PHOTO_BORDER))/2;
-    var frame_h = (scaled.h - (3*PHOTO_BORDER))/2;
-    var frame = this.canvas.rect(frame_x, frame_y, frame_w, frame_h);
-    frame.attr({'fill': 'black'});
-    
-    frame = frame.clone();
-    frame.translate(frame_w + PHOTO_BORDER, 0);
-    
-    frame = frame.clone();
-    frame.translate(0, frame_h + PHOTO_BORDER);
-    
-    frame = frame.clone();
-    frame.translate(-(frame_w + PHOTO_BORDER), 0);
-
-}
-
-function countdown(expected) {
+/**
+ * Wrapper around snap()
+ * Will count a 3-2-1 countdown before snap() is invoked.
+ */
+CameraUtils.countdown = function(expected) {
     console.log('countdown with expected time of: '+expected);
     var counter = 3;
     var countdownTimer = setInterval(function() {
         console.log(counter);
+        p.modalMessage(counter);
         if (counter == 1) {
             console.log('expected to snap: ' + expected);
             clearInterval(countdownTimer);
-            setTimeout(function() {snap(expected);}, 1000);
+            setTimeout(function() {
+                CameraUtils.snap(expected);
+            }, 1000);
         }
         counter -= 1;
     }, 1000);
 }
 
-function snap(expected_time) {
-    var now = (new Date()).getTime();
-    console.log('snap at ' + now);
-    console.log('delta from expected: ' + (expected_time - now));
-}
-
 $(window).ready(function () {
     $('button#start-button').click(function(e) {
-        $.get('time_now', null, function(data) {
+        $.get('start_snap', null, function(data) {
             // Temp logging
             console.log("set_id is: "+data.set_id);
             console.log("timestamps are: "+data.timestamps);
+            
+            p.modalMessage('Ready?', 3000);
 
             // Set global
             var timestamps = data.timestamps;
@@ -95,7 +189,7 @@ $(window).ready(function () {
                 console.log('next expected time to snap at is:' + closest_ts);
                 // Start countdown
                 setTimeout(function(expect) {
-                    countdown(expect);
+                    CameraUtils.countdown(expect);
                 }, ts_delta, closest_ts);
                 closest_ts = timestamps.pop();
                 ts_delta = closest_ts - time_now - 4000;
@@ -106,5 +200,6 @@ $(window).ready(function () {
 
     p = new PhotoView();
     p.render();
+
 });
 
