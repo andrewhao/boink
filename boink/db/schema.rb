0689fa9138379ad9b9e138e07199134752633166
Andrew Hao <andrewhao@gmail.com>
2011-08-03 13:03:29 -0700

    Added the real delayed_jobs migration, thanks to http://8raystech.com/2009/2/28/background-job-processing-in-rails-with-delayed_job.

--- boink/db/schema.rb
+++ boink/db/schema.rb
@@ -12,6 +12,19 @@
 
 ActiveRecord::Schema.define(:version => 20110803180802) do
 
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "photo_sets", :force => true do |t|
     t.string   "paths"
     t.datetime "created_at"
