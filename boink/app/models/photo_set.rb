aa053e1390337733251803c6b6aab0f8eb1243ce
Glen <Glen@Glens-MacBook-Pro.local>
2011-08-05 01:20:05 -0700

    added gem rmagick, added static method example to photo_set for compositing

--- boink/app/models/photo_set.rb
+++ boink/app/models/photo_set.rb
@@ -1,6 +1,12 @@
 require 'json'
+require 'rmagick'
+include Magick
 
 class PhotoSet < ActiveRecord::Base
+  IMAGE_HEIGHT = 400
+  IMAGE_WIDTH = 600
+  IMAGE_PADDING = 20
+  
   # Query for a JPEG image reference in the model.
   def get_image_path(index)
     return (self.paths) ? JSON.parse(self.paths)[index.to_s] : nil
@@ -28,5 +34,29 @@ class PhotoSet < ActiveRecord::Base
   # Note no trailing slash.
   def get_url_folder_path
     return "/images/sets/#{self.id}"
+  end
+  
+  def self.get_overlay_path
+    return "#{Rails.public_path}/images/overlay.png"
+  end
+  
+  # Takes the photos in this PhotoSet and composites them with RMagick
+  def self.composite_photos
+    tmp_path = "#{Rails.public_path}/images/"
+    comp = Image.new(IMAGE_WIDTH * 2 + IMAGE_PADDING * 3, IMAGE_HEIGHT * 2 + IMAGE_PADDING * 3) { self.background_color = "white" }
+    
+    tl_photo = ImageList.new(tmp_path + "photos/1.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
+    tr_photo = ImageList.new(tmp_path + "photos/2.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
+    bl_photo = ImageList.new(tmp_path + "photos/3.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
+    br_photo = ImageList.new(tmp_path + "photos/4.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
+    
+    overlay = ImageList.new(tmp_path + "overlay.png")
+    
+    comp = comp.composite(tl_photo, IMAGE_PADDING, IMAGE_PADDING, OverCompositeOp)
+    comp = comp.composite(tr_photo, IMAGE_WIDTH + IMAGE_PADDING * 2, IMAGE_PADDING, OverCompositeOp)
+    comp = comp.composite(bl_photo, IMAGE_PADDING, IMAGE_HEIGHT + IMAGE_PADDING * 2, OverCompositeOp)
+    comp = comp.composite(br_photo, IMAGE_WIDTH + IMAGE_PADDING * 2, IMAGE_HEIGHT + IMAGE_PADDING * 2, OverCompositeOp)
+    comp = comp.composite(overlay, ((comp.columns - overlay.columns)/2).round, ((comp.rows - overlay.rows)/2).round, OverCompositeOp)
+    comp.write(tmp_path + "photos/gen.jpg")
   end
 end
--- boink/db/migrate/20110725060930_create_photo_sets.rb
+++ boink/db/migrate/20110725060930_create_photo_sets.rb
@@ -2,7 +2,8 @@ class CreatePhotoSets < ActiveRecord::Migration
   def self.up
     create_table :photo_sets do |t|
       t.string :paths
-
+      t.string :final_path
+      t.boolean :printed, :default => false
       t.timestamps
     end
   end
