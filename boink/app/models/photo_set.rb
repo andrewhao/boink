e10b6cab4c987ade979643550615adb0fbe13881
Andrew Hao <andrewhao@gmail.com>
2011-08-06 13:26:17 -0700

    Classmethod -> Instance method.

--- boink/app/models/photo_set.rb
+++ boink/app/models/photo_set.rb
@@ -36,27 +36,29 @@ class PhotoSet < ActiveRecord::Base
     return "/images/sets/#{self.id}"
   end
   
-  def self.get_overlay_path
-    return "#{Rails.public_path}/images/overlay.png"
-  end
-  
   # Takes the photos in this PhotoSet and composites them with RMagick
-  def self.composite_photos
-    tmp_path = "#{Rails.public_path}/images/"
+  # Creates an image called "gen.jpg" in the set folder.
+  def composite_photos
+    #tmp_path = "#{Rails.public_path}/images/"
     comp = Image.new(IMAGE_WIDTH * 2 + IMAGE_PADDING * 3, IMAGE_HEIGHT * 2 + IMAGE_PADDING * 3) { self.background_color = "white" }
     
-    tl_photo = ImageList.new(tmp_path + "photos/1.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
-    tr_photo = ImageList.new(tmp_path + "photos/2.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
-    bl_photo = ImageList.new(tmp_path + "photos/3.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
-    br_photo = ImageList.new(tmp_path + "photos/4.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
+    tl_photo = ImageList.new("#{self.get_folder_path}/boink_0.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
+    tr_photo = ImageList.new("#{self.get_folder_path}/boink_1.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
+    bl_photo = ImageList.new("#{self.get_folder_path}/boink_2.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
+    br_photo = ImageList.new("#{self.get_folder_path}/boink_3.jpg").resize_to_fill(IMAGE_WIDTH, IMAGE_HEIGHT)
     
-    overlay = ImageList.new(tmp_path + "overlay.png")
+    overlay = ImageList.new(PhotoSet.get_overlay_path)
     
     comp = comp.composite(tl_photo, IMAGE_PADDING, IMAGE_PADDING, OverCompositeOp)
     comp = comp.composite(tr_photo, IMAGE_WIDTH + IMAGE_PADDING * 2, IMAGE_PADDING, OverCompositeOp)
     comp = comp.composite(bl_photo, IMAGE_PADDING, IMAGE_HEIGHT + IMAGE_PADDING * 2, OverCompositeOp)
     comp = comp.composite(br_photo, IMAGE_WIDTH + IMAGE_PADDING * 2, IMAGE_HEIGHT + IMAGE_PADDING * 2, OverCompositeOp)
     comp = comp.composite(overlay, ((comp.columns - overlay.columns)/2).round, ((comp.rows - overlay.rows)/2).round, OverCompositeOp)
-    comp.write(tmp_path + "photos/gen.jpg")
+    comp.write(self.get_folder_path + "/gen.jpg")
+  end
+
+  # Classmethod to return the absolute overlay path
+  def self.get_overlay_path
+    return "#{Rails.public_path}/images/overlay.png"
   end
 end
